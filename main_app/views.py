72c3b705e0a1996efcc9f084b7bd1baf9ef360a7
xiokevc <xiokevc@gmail.com>
2025-09-12 07:59:38 -0400

    django templates

--- main_app/views.py
+++ main_app/views.py
@@ -10,4 +10,4 @@ def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the CatCollector</h1>')  
+    return render(request, 'about.html')
